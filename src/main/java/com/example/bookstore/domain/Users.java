0891ca701c430638d2cd1370c010d427811fa049
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-08-20 10:01:42 +0300

    Created User class service and rep

---
+++ src/main/java/com/example/bookstore/domain/Users.java
@@ -0,0 +1,26 @@
+package com.example.bookstore.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String role;
+
+
+}
