9b6d2672f924fe6870a1650c54cf64f3d0dd2bfb
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-08-20 09:59:48 +0300

    Created User controller

--- src/main/java/com/example/bookstore/controllers/UserController.java
+++ src/main/java/com/example/bookstore/controllers/UserController.java
@@ -1,4 +1,33 @@
 package com.example.bookstore.controllers;
 
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.responses.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
+
+
+    private final BookService bookService;
+
+    public UserController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+
+    @GetMapping("/books")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Book> showAllBooks() {
+        return bookService.showAllBooks();
+    }
+
+
 }
+
+
+
+
